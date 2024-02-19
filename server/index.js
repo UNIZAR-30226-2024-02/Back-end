df8e1808e5bd996ee5641fd4eabed217667cf817
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2024-02-19 19:10:17 +0100

    Conexión probada con URL de prueba y eliminado deprecado
    - He puesto una connection URL de una BD mia de prueba que me da igual que sea públia la password (es temporal y además solo puedo entrar yo con mi IP) -> Cuando tengamos la buena sustituirla y utilizar github secrets cuando salga a producción
    - Eliminados parámetros deprecados (https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// db connection
+connectDB();
 
 // ruta prueba principal
 app.get('/', (req, res) => {
