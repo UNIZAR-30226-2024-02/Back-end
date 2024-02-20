131e228d2bdd42d308fb9d09e3dfb1df8aae97dc
Fabián Murgado <fabianmurgado@gmail.com>
2024-02-20 01:23:42 +0100

    Definición inicial de los modelos de mongoose.

--- src/models/Chat.js
+++ src/models/Chat.js
@@ -1,9 +1,19 @@
-const mongoose = require("mongoose")
-const Mensaje = require("./Mensaje.js")
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-new Chat = mongoose.Schema({
-  nombreChat: {type: String, require:true},
-  Mensajes: {type: [Mensaje], default: [], require: true}
-})
+const MensajeSchema = new Schema({
+  Texto: { type: String, required: true, default: "" },
+  idUsuario: {
+    type: Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true
+  },
+  Timestamp: { type: Date, default: () => Date.now(), required: true }
+});
 
-module.exports = mongoose.model("Chat", ChatSchema)
+const ChatSchema = new Schema({
+  nombreChat: { type: String, required: true },
+  mensajes: [MensajeSchema]
+});
+
+module.exports = mongoose.model("Chat", ChatSchema);
---
+++ src/models/Partida.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const TerritorioSchema = mongoose.Schema({
+    Nombre: [{ type: String }],
+    Frontera: [{ type: String }],
+    Tropas: { type: Number, required: true, default: 0 }
+});
+
+const CartaSchema = mongoose.Schema({
+    Territorio: [{ type: String }],
+    Tropas: { type: Number, required: true, default: 0 }
+});
+
+const ContinenteSchema = mongoose.Schema({
+    Territorios: [{ type: TerritorioSchema }],
+    Valor: { type: Number, required: true }
+});
+
+const JugadorSchema = mongoose.Schema({
+    Usuario: {
+        type: {
+            type: String,
+            ref: 'Usuario.idUsuario'
+        },
+    },
+    Territorios: [{ type: TerritorioSchema }],
+    Cartas: [{ type: CartaSchema }],
+    Turno: { type: Number, required: true }
+});
+
+const PartidaSchema = mongoose.Schema({
+    Nombre: { type: String, required: true },
+    Iniciada: { type: Boolean, required: true, default: false },
+    Terminada: { type: Boolean, required: true, default: false },
+    fechaInicio: { type: Date, required: true, immutable: true },
+    fechaFin: { type: Date, required: true, immutable: true },
+    Ganador: {
+        type: {
+            type: String,
+            ref: 'Usuario.idUsuario'
+        },
+    },
+    Turno: { type: Number, required: true, default: 0 },
+    Jugadores: [{ type: JugadorSchema }],
+    Mapa: [{ type: ContinenteSchema }]
+});
+
+module.exports = mongoose.model("Partida", PartidaSchema);
