0672bec29e16f6069528e8707002a9e67e811ac6
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2024-02-21 12:00:04 +0100

    Reestructuración
    - Merge de BBDD con ramas 6 y 9. Es funcional.
    - Se ha mantenido la conexión a la base de datos y se ha fusionado app.js con index.js.
    - Se ejecuta con node run dev  (ver package.json). Escucha en el puerto 4000

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: '../src/.env' });
 
 const connectDB = async () => {
     try {
--- server/src/index.js
+++ server/src/index.js
@@ -4,13 +4,21 @@ const connectDB = require('./config/db');
 const app = express();
 
 // db connection
-connectDB();
+async function startApp() {
+    try {
+        await connectDB();
+    } catch (error) {
+        console.error('Error conexión BBDD', error);
+    }
+}
 
 // ruta prueba principal
 app.get('/', (req, res) => {
     res.send('Todo funciona bien');
 })
 
+
+startApp();
 app.listen(4000, () => {
     console.log('todo piola xD');
 })
