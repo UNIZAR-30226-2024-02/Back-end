ea8204752142c6cac927ee3352b8319963ef554a
737275 <146024014+737275@users.noreply.github.com>
2024-02-28 21:30:59 +0100

    Added auth token
    Implementado un token de una librería de autentificación para mantener la sesión.
    
    Co-Authored-By: Juan Almodóvar <86721752+Juanguppy@users.noreply.github.com>

--- server/src/queries/login.js
+++ server/src/queries/login.js
@@ -3,7 +3,7 @@ const Usuario = require("../models/Usuario")
 async function login(username, password, email) {
     // Comprobar si el usuario o el correo electrónico ya existen en la base de datos
     const existingUser = await Usuario.findOne({ $or: [{ idUsuario: username }, { correo: email }] });
-    return existingUser && existingUser.pass === password;
+    return { valid: existingUser && existingUser.pass === password, idUsuario: existingUser && existingUser.idUsuario };
 }
 
 module.exports = login
--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const login = require('../queries/login');
+const jwt = require('jsonwebtoken');
 
 // petición post desde <url>/register desencadena esta acción
 // los parámetros van en el body de la request
@@ -17,8 +18,9 @@ router.post('/', async (req, res) => {
         username = id
       }
       const succ = await login(username, password, email)
-      if (succ) {
-        res.status(200).json({ message: 'Login correcto' })
+      if (succ.valid) {
+        const token = jwt.sign({ idUsuario: succ.idUsuario }, 'claveSecreta', { expiresIn: '1h' });
+        res.status(200).json({ message: 'Login correcto', token })
       } else {
         res.status(403).json({ message: 'Credenciales incorrectas' })
       }
