b1cc28627e02c5aab59e18280150c776a1d04973
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2024-03-22 18:52:38 +0100

    Socket.io
    - He configurado socket.io para que admita cors
    - He realizado unas pruebas que reciben peticiones del front para unirse a diferentes "grupos"
    - He realizado una función y una prueba que, pasados 5000 segundos, envía una notificación at odos los jugadores de un grupo. He probado en el front que funciona

--- server/src/app.js
+++ server/src/app.js
@@ -10,8 +10,16 @@ const tiendaRouter = require('./routes/tienda');
 const chatRouter = require('./routes/chats')
 const misSkinsRouter = require('./routes/misSkins')
 const cors = require('cors');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "http://localhost:4200",
+      methods: ["GET", "POST"]
+    }
+  });
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
@@ -23,7 +31,7 @@ app.use(express.json())
 async function startApp() {
     try {
         await connectDB();
-        server = app.listen(4000, () => {
+        server.listen(4000, () => {
             console.log('Todo piola xD');
         });
     } catch (error) {
@@ -69,5 +77,38 @@ app.use('/misSkins', misSkinsRouter)
 app.get('/', (req, res) => {
     res.send('Todo funciona bien');
 })
+
+io.on('connection', (socket) => {
+    const clientIp = socket.handshake.address; // Obtener la dirección IP del cliente
+    console.log(`Usuario con IP ${clientIp} conectado`); // Registrar la IP del cliente
+
+    socket.on('joinGame', (gameId) => {
+        socket.join(gameId);
+        console.log(`Usuario se unió a la partida ${gameId}`);
+        console.log(`IP del cliente: ${clientIp}`);
+    });
+
+    socket.on('friendRequest', (data) => {
+        io.to(data.userId).emit('friendRequest', data.notification);
+        console.log(`Notificación de solicitud de amistad enviada a ${data.userId}`);
+        console.log(`IP del cliente: ${clientIp}`);
+    });
+
+    socket.on('disconnectGame', (gameId) => {
+        console.log(`Usuario desconectado de la partida ${gameId}`);
+        console.log(`IP del cliente: ${clientIp}`);
+    });
+
+});
+
+function notifyGame(gameId, notification) {
+    io.to(gameId).emit('gameNotification', notification);
+    console.log(`Notificación de juego enviada a la partida ${gameId}`);
+}
+
+setTimeout(() => {
+    notifyGame('game123', 'Mensaje de notificación para los usuarios de la partida');
+}, 5000);
+
 
 module.exports = { app, startApp, close };
