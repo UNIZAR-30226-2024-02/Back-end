9c57eb2310282b45e1c4fa12de141bfd40e5d738
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2024-03-24 13:01:35 +0100

    ajustes hechos

--- server/src/unitTestLinux.sh
+++ server/src/unitTestLinux.sh
@@ -2,8 +2,18 @@
 
 cd test
 
+exit_code=0
+
 for file in *; do
     if [ -f "$file" ] && [ -x "$file" ]; then
         npx jest "$file"
+        
+        code=$?
+
+        if [ $code -ne 0 ]; then
+            exit_code=1
+        fi
     fi
 done
+
+exit $exit_code
