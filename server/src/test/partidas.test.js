eac44b55c01d8b0131f8a3a3b78e70e6a66ba2e3
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2024-05-17 14:02:30 +0200

    Arreglados test unitarios
    - Se habían eliminado los usuarios de prueba de la BD, por lo que los test unitarios fallaban en GitHub.

--- server/src/test/partidas.test.js
+++ server/src/test/partidas.test.js
@@ -181,7 +181,7 @@ describe('Unir a partidas', () => {
         const loginCredentials = {
             id: 'a',
             password: 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb'
-        };
+        }; 
 
         const response = await request
             .post('/login')
